703d0f8d659dffe0f0e283968af812bc27df82d5
Uriel De Carlini <uriel@acuidado>
2021-04-02 10:12:10 -0300

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Services } from "./components/services";
+import { Gallery } from "./components/gallery";
+import { Testimonials } from "./components/testimonials";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+
+const Home = ({ landingPageData }) => {
+  return (
+    <Grid>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </Grid>
+  );
+};
+
+export default Home;
