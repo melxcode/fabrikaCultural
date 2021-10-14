a4f475de5e489fa0738cd6264392ecb27e491375
Uriel De Carlini <uriel@acuidado>
2021-10-14 19:29:23 -0300

    fix number again

--- src/data/datos.js
+++ src/data/datos.js
@@ -1,3 +1,3 @@
-export const WHATSAPP_NUMBER = "2664482403";
+export const WHATSAPP_NUMBER = "+5492664482403";
 export const PLACEHOLDER_IMG = "https://i.ibb.co/JRzzDNB/placeholder.png";
 export const BIG_LOGO = "/img/fondo.jpeg";
